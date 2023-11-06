52d1463561cac48fdca82f7f84689095205fc187
giang-1 <giang06121102@gmail.com>
2023-11-06 21:01:48 +0700

    add javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("External html");
+let a = 6;
+let b = 25;
+let c = a + b;
+console.log("tong la:", c);
+//prompt("nhập số: ");// hàm dùng để nhập dữ liệu
+//alert("tong la: " + c );
+//confirm("bạn có muốn nhập con số này không?");//thông báo xác nhận vấn đề
+//in trên trang web: document.write
+//document.write("tổng là: " + c)
